05fe42b427bfb4d4a9e5df2279d0d8023f56e6cd
Leon Si <leon@leonsilicon.com>
2026-03-30 15:34:14 -0400

    remove import meta

--- packages/types/src/default.d.ts
+++ packages/types/src/default.d.ts
@@ -1,5 +1,4 @@
 // @ts-ignore: Will point to llrt-types/index.d.ts in the built exports/ directory
 /// <reference types="./index.d.ts" />
 /// <reference types="./chord.d.ts" />
-/// <reference types="./import-meta.d.ts" />
 export {};
